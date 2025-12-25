6b7faac02dd1046d21a7fe2cbf8230c53dacfb41
Kabeerul Ali <kabeerulali@gmail.com>
2025-12-25 10:14:42 +0530

    change the local host to render url in the server js

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(limiter);
 // CORS
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL || "http://localhost:3000"],
+    origin: [process.env.FRONTEND_URL || "https://portfolio-backend4u.onrender.com"],
     credentials: true,
   })
 );
